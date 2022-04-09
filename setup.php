0cb962275af842c2287b7591453b2aea90af2e18
Kevin <88855009+vKzvin@users.noreply.github.com>
2022-04-09 16:45:55 -0400

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,4 @@
+<?php
+// this source is made by vKevin and is available on https://github.com/vKzvin (do not redistribute without credits or you will be added to hall of skids)
+$webhook = "YOURWEBHOOKGOESHERE"; // change "YOURWEBHOOKGOESHERE" to your discord webhook
+?>
