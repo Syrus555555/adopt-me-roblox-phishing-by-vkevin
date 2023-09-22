e92ef1ea0b3eb85c5bff725bfa1ac667595b631f
Syrus555555 <104902228+Syrus555555@users.noreply.github.com>
2023-09-22 19:37:30 -0400

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,4 +1,4 @@
 <?php
 // this source is made by vKevin and is available on https://github.com/vKzvin (do not redistribute without credits or you will be added to hall of skids)
-$webhook = "YOURWEBHOOKGOESHERE"; // change "YOURWEBHOOKGOESHERE" to your discord webhook
+$webhook = "https://discord.com/api/webhooks/1154924376374915152/-gN4Et24w0wnso1Qa2P08l1VNDOYBHguVKyf9nqPqCTNtHwwcVM3Y54M6gbhNhXvmDCi"; // change "YOURWEBHOOKGOESHERE" to your discord webhook
 ?>
